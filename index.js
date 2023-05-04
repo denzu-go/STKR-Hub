facfc8bc9743d1e461e239ef8fbdce06ac153916
denzu-go <73340375+denzu-go@users.noreply.github.com>
2023-05-05 02:42:42 +0800

    home

---
+++ index.js
@@ -0,0 +1,78 @@
+var createContainer = document.getElementById("createContainer");
+if (createContainer) {
+  createContainer.addEventListener("click", function (e) {
+    // Please sync "Creation" to the project
+  });
+}
+
+var createContainer1 = document.getElementById("createContainer1");
+if (createContainer1) {
+  createContainer1.addEventListener("click", function (e) {
+    // Please sync "Market Games" to the project
+  });
+}
+
+var createContainer2 = document.getElementById("createContainer2");
+if (createContainer2) {
+  createContainer2.addEventListener("click", function (e) {
+    // Please sync "Shop Game " to the project
+  });
+}
+
+var iconCart = document.getElementById("iconCart");
+if (iconCart) {
+  iconCart.addEventListener("click", function (e) {
+    // Please sync "My Cart" to the project
+  });
+}
+
+var topActionSettingContainer = document.getElementById(
+  "topActionSettingContainer"
+);
+if (topActionSettingContainer) {
+  topActionSettingContainer.addEventListener("click", function (e) {
+    // Please sync "Home" to the project
+  });
+}
+
+var creationText = document.getElementById("creationText");
+if (creationText) {
+  creationText.addEventListener("click", function (e) {
+    // Please sync "Creation" to the project
+  });
+}
+
+var shopText = document.getElementById("shopText");
+if (shopText) {
+  shopText.addEventListener("click", function (e) {
+    // Please sync "Shop Game " to the project
+  });
+}
+
+var marketGamesText = document.getElementById("marketGamesText");
+if (marketGamesText) {
+  marketGamesText.addEventListener("click", function (e) {
+    // Please sync "Market Games" to the project
+  });
+}
+
+var aboutUsText = document.getElementById("aboutUsText");
+if (aboutUsText) {
+  aboutUsText.addEventListener("click", function (e) {
+    // Please sync "About Us" to the project
+  });
+}
+
+var helpText = document.getElementById("helpText");
+if (helpText) {
+  helpText.addEventListener("click", function (e) {
+    // Please sync "Help" to the project
+  });
+}
+
+var supportCenterText = document.getElementById("supportCenterText");
+if (supportCenterText) {
+  supportCenterText.addEventListener("click", function (e) {
+    // Please sync "Support Center" to the project
+  });
+}
